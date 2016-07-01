bbc75863200e2ed086d6da96d4b42f09f2687bb0
Niket Pathak <niketpathak89@gmail.com>
2016-07-02 00:23:28 +0200

    fix jquery implementation, main-menu active class management, home-page link fix

--- wp-content/themes/angular-bootstrap/js/vanilla_jq.js
+++ wp-content/themes/angular-bootstrap/js/vanilla_jq.js
@@ -1,9 +1,10 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-	var main_menu = document.getElementById("main-menu");
-	var main_menu_items  = document.getElementsByClassName("menu-item");
-	main_menu_items.addEventListener("click", menuItemCLick);	//this event works only with getElementbyId();
-});
-
-function menuItemCLick(){
-	console.log("clicked>>.");
-}
+//Main-menu: add/remove active class
+(function($) {
+	$(document).ready(function(){
+		$(".menu-item a").click(function(){
+			$(".menu-item").removeClass("active");
+			$(this).parent().addClass("active");
+			//return false;
+		});
+	});
+}(jQuery));
