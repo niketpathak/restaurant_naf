b44a364f6370375126448a7ba28ef8cec83a9c68
Niket Pathak <niketpathak89@gmail.com>
2016-07-10 01:51:02 +0200

    added sticky header + styling

--- wp-content/themes/angular-bootstrap/js/vanilla_jq.js
+++ wp-content/themes/angular-bootstrap/js/vanilla_jq.js
@@ -1,10 +1,27 @@
 //Main-menu: add/remove active class
 (function($) {
 	$(document).ready(function(){
+
+		// **** menu active items
 		$(".menu-item a").click(function(){
 			$(".menu-item").removeClass("active");
 			$(this).parent().addClass("active");
 			//return false;
 		});
-	});
+
+		// **** sticky nav
+		var stickyNavTop = $('#masthead').offset().top;
+		$(window).scroll(function() {
+			var scrollTop = $(window).scrollTop();
+			if (scrollTop > stickyNavTop) {
+				$('#masthead').addClass('sticky');
+				$('.outermost').css('margin-top',"120px");
+
+			} else {
+				$('#masthead').removeClass('sticky');
+				$('.outermost').css('margin-top',"0px");
+			}
+		});
+
+	}); // doc.ready fn
 }(jQuery));
