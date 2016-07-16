5a3fcdee327d70fcb114236134f2dc17628efaa4
Niket Pathak <niketpathak89@gmail.com>
2016-07-16 14:49:49 +0200

    integrated underscore.js library!

--- wp-content/themes/angular-bootstrap/functions.php
+++ wp-content/themes/angular-bootstrap/functions.php
@@ -129,6 +129,8 @@ function _tk_scripts() {
 	wp_enqueue_script('angularjs-route', get_template_directory_uri() .'/node_modules/angular-route/angular-route.min.js');
 	wp_enqueue_script('angularjs-sanitize', get_stylesheet_directory_uri() . '/node_modules/angular-sanitize/angular-sanitize.min.js');
 	wp_enqueue_script('angularjs-filter', get_stylesheet_directory_uri() . '/node_modules/angular-filter/dist/angular-filter.min.js');
+	//Load Underscore lib
+	wp_enqueue_script('underscore-lib', get_stylesheet_directory_uri() . '/node_modules/underscore/underscore-min.js');
 	//Load App
 	wp_enqueue_script('app_restaurant', get_stylesheet_directory_uri() . '/js/app.js', array( 'angularjs', 'angularjs-route' ));
 	wp_enqueue_script('ThemeService', get_stylesheet_directory_uri() . '/js/services/ThemeService.js');
