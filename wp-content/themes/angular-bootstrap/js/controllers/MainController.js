56d6dfbda827ad2ec40c152dec97c01314a2cd00
Niket Pathak <niketpathak89@gmail.com>
2016-07-27 00:26:32 +0200

    added styling for homepage.

--- wp-content/themes/angular-bootstrap/js/controllers/MainController.js
+++ wp-content/themes/angular-bootstrap/js/controllers/MainController.js
@@ -1,12 +1,12 @@
 //Main controller
 app.controller('MainController', ['$scope', 'ThemeService', function($scope, ThemeService) {
 	//Get Categories from ThemeService
-	ThemeService.getAllCategories();
+	// ThemeService.getAllCategories();
 	
 	//Get the first page of posts from ThemeService
-	ThemeService.getPosts(1);
+	// ThemeService.getPosts(1);
 
-	$scope.data = ThemeService;
+	// $scope.data = ThemeService;
 	$scope.doc_root = doc_root;
 	console.log("Inside Main-controller");
 
