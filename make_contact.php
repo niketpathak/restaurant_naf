cdbba7c41bc81ea19f219d7eaecc6ccab504dcb4
Niket Pathak <niketpathak89@gmail.com>
2016-08-06 10:45:18 +0200

    contact page handler, email functionality, form styling fix

---
+++ make_contact.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: niketpathak
+ * Date: 31/07/16
+ * Time: 23:04
+ */
+
+//decode json encoded post data
+$params = json_decode(file_get_contents('php://input'),true);
+//print_r($params);
+$output = array("res"=>0, "msg"=>"Missing Input");     //output array
+
+//check for empty post paramters
+if(empty($params["name"]) || empty($params["email"]) || empty($params["sub"])) {
+    exit(json_encode($output));
+}
+
+//Valide email
+if (!filter_var($params["email"], FILTER_VALIDATE_EMAIL)) {
+    $output["msg"] = "Invalid Email Address";
+    exit(json_encode($output));
+}
+
+require_once 'wp-content/themes/angular-bootstrap/lib/PHPMailer/PHPMailerAutoload.php';
+$message = file_get_contents("wp-content/themes/angular-bootstrap/lib/email_template/template.html");
+$message = str_ireplace("{%sender_email%}", $params["email"],$message);
+$message = str_ireplace("{%sender_subject%}", $params["sub"],$message);
+$message = str_ireplace("{%sender_name%}", htmlspecialchars($params["name"]),$message);
+$message = str_ireplace("{%sender_msgg%}", htmlspecialchars($params["msg"]),$message);
+
+//echo $message;
+
+
+$mailsubject = "EscaleIndienne:".$params["sub"];
+$rec_email = 'niketpathak89@gmail.com';
+$array_content=array("post_email" => $params["email"], "post_name" => $params["name"], "msg" => $message);
+$res = send_a_mail($array_content, $rec_email, $mailsubject);
+
+if ($res) {
+    $output["res"] = "1";
+    $output["msg"] = "Thank you for contacting us. We will revert to you ASAP.";
+} else {
+    $output["msg"] = "Error. Server Temporarily Unavailable";
+
+}
+
+exit(json_encode($output));
+
+
+
+
+
+
+
+
+function send_a_mail($var_array, $destinationmail, $mailsubject) {
+    $mail = new PHPMailer();
+	$mail->SMTPDebug = true;	    // uncomment to diagnose errors
+    $mail->IsSMTP();
+    $mail->Host 	= "localhost";   //  ssl://smtp.gmail.com
+    $mail->From 	= ('' != $var_array["post_email"]) ? $var_array["post_email"] : "admin@escaleindienne.com";
+    $mail->FromName = ('' != $var_array["post_name"]) ? $var_array["post_name"] : "EscaleIndienne Contant";
+    $mail->CharSet = "UTF-8";
+    $mail->AddAddress($destinationmail);	//recepient email comes here!
+    $mail->IsHTML(true);
+    $mail->Subject = $mailsubject;
+
+    $mail->Body=$var_array["msg"];
+    if ( ! $mail->Send())
+    {
+        $result = $mail->ErrorInfo;
+    }
+    else
+    {
+        $result = TRUE;
+    }
+
+    return $result;
+}
--- wp-content/themes/angular-bootstrap/js/controllers/PageController.js
+++ wp-content/themes/angular-bootstrap/js/controllers/PageController.js
@@ -1,7 +1,41 @@
 //Page Controller
 app.controller('pageController',
-	['$scope', '$http', '$routeParams', '$sce', 'CachePagesService', function($scope, $http, $routeParams, $sce, CachePagesService) {
+	['$scope', '$http', '$routeParams', '$sce', 'CachePagesService', '$timeout', function($scope, $http, $routeParams, $sce, CachePagesService, $timeout) {
 		$scope.doc_root = doc_root;
+		/************** specific to contact page start *************/
+		$scope.rest_stat = "";
+		$scope.rest_stat_hide = true;
+		$scope.reservation_forceDisable = false;
+		$scope.ajax_loader_hide = true;
+		//handle form submit
+		$scope.submitContactRequest = function () {
+			$scope.contact_forceDisable = true;
+			$scope.ajax_loader_hide = false;
+			var data_in = {
+				"name": $scope.name,
+				"email": $scope.email,
+				"sub": $scope.subject,
+				"msg": $scope.msg
+			};
+			$http.post(doc_root+'/make_contact.php', data_in).then(function(e){
+				// console.log('success fired',e);
+				$scope.ajax_loader_hide = true;
+				$scope.rest_stat_hide = false;
+				if(e.data.res==0) {
+					$scope.reservation_forceDisable = false;
+				}
+				$scope.rest_stat = e.data.msg;
+				$timeout(function () {
+					$scope.rest_stat = "";
+					$scope.rest_stat_hide = true;
+				}, 4000);
+			}, function(e){
+				//@to-do: handle failure of ajax req
+				return;
+			});
+		}
+		/************** specific to contact page end *************/
+
 		if(CachePagesService.cached === false) {
 					//FirstRun: receives a Promise
 					CachePagesService.getAllPages().then(function(){
